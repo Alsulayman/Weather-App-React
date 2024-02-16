6e51682f6bb7af352b390607ecd674e4c8e72f5c
Alsulayman <radwansulimann@gmail.com>
2024-02-16 18:10:25 +0100

    Integrate Material-UI components to create a responsive navbar.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Navbar from "./Navbar";
 
 const App = () => {
   // State to hold the input value for the city
@@ -11,7 +12,7 @@ const App = () => {
   const [error, setError] = useState("");
 
   // API key for OpenWeatherMap
-  const api = "";
+  const api = "681676c6a80991562750999f64af1221";
 
   // API URL constructed with the city input value and the API key
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${api}&units=metric`;
@@ -38,67 +39,82 @@ const App = () => {
       setWeatherData(null);
       setError("No City was found on the Server");
     }
+    console.log(weatherData);
   };
 
   return (
-    <div className="app" style={{ backgroundColor: "rgb(225, 93, 68)" }}>
-      <div className="container">
-        <h1>LIVE° Weather</h1>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            id="city-name"
-            placeholder="Enter City"
-            value={cityValue}
-            onChange={(e) => setCityValue(e.target.value)}
-            className="city-input"
-          />
-          <input type="submit" value="Get Weather" className="submit-button" />
-        </form>
-        <div id="main-div">
-          {weatherData && (
-            <>
-              <div className="icon">
+    <>
+      <Navbar
+        weatherData={weatherData}
+        api={api}
+        apiUrl={apiUrl}
+        handleSubmit={handleSubmit}
+        cityValue={cityValue}
+        setCityValue={setCityValue}
+      />
+      <div className="app">
+        <div className="container">
+          <h1>LIVE° Weather</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              id="city-name"
+              placeholder="Enter City"
+              value={cityValue}
+              onChange={(e) => setCityValue(e.target.value)}
+              className="city-input"
+            />
+            <input
+              type="submit"
+              value="Get Weather"
+              className="submit-button"
+            />
+          </form>
+          <div id="main-div">
+            {weatherData && (
+              <>
+                <div className="icon">
+                  <img
+                    src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
+                    alt="Weather Icon"
+                  />
+                </div>
+                <div
+                  className="description"
+                  style={{ color: "rgb(252, 252, 252)" }}
+                >
+                  {weatherData.weather[0].description}
+                </div>
+                <br />
+                <div className="temperature">
+                  Temperature in Celsius: {weatherData.main.temp} °C
+                </div>
+                <div className="kelvin">
+                  Temperature in Kelvin: {weatherData.main.temp + 273.15} k
+                </div>
+                <div className="fahrenheit">
+                  Temperature in Fahrenheit:{" "}
+                  {(weatherData.main.temp * 9) / 5 + 32} °F
+                </div>
+                <div className="details">
+                  Humidity is: {weatherData.main.humidity}%
+                </div>
+              </>
+            )}
+            {error && (
+              <div className="error">
+                {error}
                 <img
-                  src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
-                  alt="Weather Icon"
+                  src="https://cdn-icons-png.flaticon.com/512/5201/5201284.png"
+                  width="50px"
+                  height="50px"
                 />
               </div>
-              <div
-                className="description"
-                style={{ color: "rgb(252, 252, 252)" }}
-              >
-                {weatherData.weather[0].description}
-              </div>
-              <br />
-              <div className="temperature">
-                Temperature in Celsius: {weatherData.main.temp} °C
-              </div>
-              <div className="kelvin">
-                Temperature in Kelvin: {weatherData.main.temp + 273.15} k
-              </div>
-              <div className="fahrenheit">
-                Temperature in Fahrenheit:{" "}
-                {(weatherData.main.temp * 9) / 5 + 32} °F
-              </div>
-              <div className="details">
-                Humidity is: {weatherData.main.humidity}%
-              </div>
-            </>
-          )}
-          {error && (
-            <div className="error">
-              {error}
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/5201/5201284.png"
-                width="50px"
-                height="50px"
-              />
-            </div>
-          )}
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
